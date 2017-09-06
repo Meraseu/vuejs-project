acd50bb822b3af444d7f0405e0a106457a044ae7
meraseudev <meraseudev@gmail.com>
2017-09-06 17:54:34 +0900

    firebase 연동 및 signup 구현

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 
 Vue.use(Vuetify)
@@ -10,5 +11,14 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCCVozh75B_37lKlONUQtvc1NEAKlV9CJM',
+      authDomain: 'vuejs-project-3b5b6.firebaseapp.com',
+      databaseURL: 'https://vuejs-project-3b5b6.firebaseio.com',
+      projectId: 'vuejs-project-3b5b6',
+      storageBucket: ''
+    })
+  }
 })
