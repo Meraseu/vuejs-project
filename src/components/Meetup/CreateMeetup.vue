d25f69fb35fab3a6e61cf0aab9ff1418f0bc50ac
meraseudev <meraseudev@gmail.com>
2017-09-08 18:41:53 +0900

    게시물 등록 작업 중

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,63 @@
 <template>
-  <div>
-    <p>The Create Meetup Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4>Create a new Meetup</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field name="title" label="Title" id="title" required v-model="title"></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field name="location" label="Location" id="location" required v-model="location"></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field name="imageUrl" label="Image URL" id="image-url" required v-model="imageUrl"></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" width="100%">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field name="description" label="Description" id="description" required multi-line v-model="description"></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary" :disabled="!formIsValid">Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: '',
+        location: '',
+        imageUrl: '',
+        description: ''
+      }
+    },
+    computed: {
+      formIsValid () {
+        return this.title !== '' && this.location !== '' && this.imageUrl !== '' && this.description !== ''
+      }
+    }
+  }
+</script>
